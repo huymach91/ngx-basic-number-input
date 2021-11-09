6668df8f4ae217cfc617af769bc0785ea1cc8109
huymach91 <huy.mach.webdev@gmail.com>
2021-11-09 15:05:06 +0700

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
+import { INumberInputOptional } from './ngx-basic-number-input/ts-number-input';
 
 @Component({
   selector: 'my-app',
@@ -10,6 +11,11 @@ export class AppComponent implements OnInit {
   form = this.formBuilder.group({
     number: this.formBuilder.control(''),
   });
+
+  public numberInputConfig: INumberInputOptional = {
+    separator: ',',
+    fractionDigits: 1,
+  };
 
   constructor(private formBuilder: FormBuilder) {}
 
